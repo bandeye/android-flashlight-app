a7517a81c6f40277524ddf2b4c30ae0e94794ad5
bandeye <gruco18@gmail.com>
2025-01-19 19:08:14 +0100

    init

--- app/src/main/java/com/example/androidflashlightapp/MainActivity.kt
+++ app/src/main/java/com/example/androidflashlightapp/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.androidflashlightapp
 
+import android.content.Context
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,9 +16,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import com.example.androidflashlightapp.ui.theme.AndroidFlashlightAppTheme
 
 class MainActivity : ComponentActivity() {
+    var flashLightStatus: Boolean = false
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -27,6 +36,28 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        openFlashLight()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun openFlashLight() {
+        val cameraManager = ContextCompat.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        val cameraId = cameraManager.cameraIdList[0]
+        if (!flashLightStatus) {
+            try {
+                cameraManager.setTorchMode(cameraId, true)
+                flashLightStatus = true
+
+            } catch (e: CameraAccessException) {
+            }
+        } else {
+            try {
+                cameraManager.setTorchMode(cameraId, false)
+                flashLightStatus = false
+            } catch (e: CameraAccessException) {
+            }
+        }
+
     }
 }
 
