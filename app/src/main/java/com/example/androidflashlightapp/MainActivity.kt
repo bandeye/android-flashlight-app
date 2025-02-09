6929ee91c4bf6beb8b6d79d911aacb3c2d9779f2
bandeye <gruco18@gmail.com>
2025-02-09 22:41:36 +0100

    ratingbar should be changed to seekbar

--- app/src/main/java/com/example/androidflashlightapp/MainActivity.kt
+++ app/src/main/java/com/example/androidflashlightapp/MainActivity.kt
@@ -1,48 +1,36 @@
 package com.example.androidflashlightapp
 
 import android.content.Context
-import android.hardware.camera2.CameraAccessException
 import android.hardware.camera2.CameraManager
 import android.os.Bundle
 import android.widget.RatingBar
-import android.widget.Switch
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 
 class MainActivity : ComponentActivity() {
-    private var flashLightStatus: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.main_layout)
-        val mySwitch: Switch = findViewById(R.id.switch1)
-        mySwitch.setOnCheckedChangeListener { _, _ ->
-            openFlashLight()
+        val myRatingBar: RatingBar = findViewById(R.id.ratingBar)
+        myRatingBar.setOnRatingBarChangeListener{ _, rating, _ ->
+            openFlashLight(true, rating.toInt())
         }
-        mySwitch.isChecked = true
+        val ratingToSet = 1
+        myRatingBar.rating = ratingToSet.toFloat()
     }
 
-    private fun openFlashLight() {
+    override fun onDestroy() {
+        super.onDestroy()
+        openFlashLight(false, 1)
+    }
+
+    private fun openFlashLight(flashLightStatus: Boolean, rating: Int) {
         val cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
         val cameraId = cameraManager.cameraIdList[0]
-        if (!flashLightStatus) {
-            try {
-                val myRatingBar: RatingBar = findViewById(R.id.ratingBar)
-                cameraManager.turnOnTorchWithStrengthLevel(cameraId, myRatingBar.rating.toInt())
-                flashLightStatus = true
-
-            } catch (e: CameraAccessException) {
-                println(e.toString())
-            }
-        } else {
-            try {
-                cameraManager.setTorchMode(cameraId, false)
-                flashLightStatus = false
-            } catch (e: CameraAccessException) {
-                println(e.toString())
-            }
-        }
+        cameraManager.turnOnTorchWithStrengthLevel(cameraId, rating)
+        cameraManager.setTorchMode(cameraId, flashLightStatus)
     }
 }
 
