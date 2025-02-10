0a28289802b1643290047a3c4782f9e9421592cd
bandeye <77682637+bandeye@users.noreply.github.com>
2025-02-10 19:10:54 +0100

    dev finally finished (#4)
    * init
    
    * ratingbar should be changed to seekbar
    
    * ratingbar changed to seekbar

--- app/src/main/java/com/example/androidflashlightapp/MainActivity.kt
+++ app/src/main/java/com/example/androidflashlightapp/MainActivity.kt
@@ -1,48 +1,74 @@
 package com.example.androidflashlightapp
 
 import android.content.Context
-import android.hardware.camera2.CameraAccessException
 import android.hardware.camera2.CameraManager
 import android.os.Bundle
-import android.widget.RatingBar
-import android.widget.Switch
+import android.view.View
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 
+
 class MainActivity : ComponentActivity() {
-    private var flashLightStatus: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.main_layout)
-        val mySwitch: Switch = findViewById(R.id.switch1)
-        mySwitch.setOnCheckedChangeListener { _, _ ->
-            openFlashLight()
-        }
-        mySwitch.isChecked = true
+
+        val seekBar = findViewById<View>(R.id.seekBar) as SeekBar
+        seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seekBar: SeekBar, progress: Int,
+                fromUser: Boolean
+            ) {
+                openFlashLight(true, progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
+
+        seekBar.progress = 1
     }
 
-    private fun openFlashLight() {
+    override fun onDestroy() {
+        super.onDestroy()
+        openFlashLight(false, 1)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        openFlashLight(false, 1)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        openFlashLight(false, 1)
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        openFlashLight(true,1)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        openFlashLight(true,1)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        openFlashLight(true, 1)
+    }
+
+    private fun openFlashLight(flashLightStatus: Boolean, rating: Int) {
         val cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
         val cameraId = cameraManager.cameraIdList[0]
-        if (!flashLightStatus) {
-            try {
-                val myRatingBar: RatingBar = findViewById(R.id.ratingBar)
-                cameraManager.turnOnTorchWithStrengthLevel(cameraId, myRatingBar.rating.toInt())
-                flashLightStatus = true
-
-            } catch (e: CameraAccessException) {
-                println(e.toString())
-            }
-        } else {
-            try {
-                cameraManager.setTorchMode(cameraId, false)
-                flashLightStatus = false
-            } catch (e: CameraAccessException) {
-                println(e.toString())
-            }
-        }
+        cameraManager.turnOnTorchWithStrengthLevel(cameraId, rating)
+        cameraManager.setTorchMode(cameraId, flashLightStatus)
     }
 }
 
