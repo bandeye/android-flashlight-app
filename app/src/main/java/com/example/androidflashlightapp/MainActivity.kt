614e1a72b05c476bf39e07e5c2fc69b0c3d1f6de
bandeye <gruco18@gmail.com>
2025-02-10 19:09:16 +0100

    ratingbar changed to seekbar

--- app/src/main/java/com/example/androidflashlightapp/MainActivity.kt
+++ app/src/main/java/com/example/androidflashlightapp/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.androidflashlightapp
 import android.content.Context
 import android.hardware.camera2.CameraManager
 import android.os.Bundle
-import android.widget.RatingBar
+import android.view.View
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+
 
 class MainActivity : ComponentActivity() {
 
@@ -13,17 +16,52 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.main_layout)
-        val myRatingBar: RatingBar = findViewById(R.id.ratingBar)
-        myRatingBar.setOnRatingBarChangeListener{ _, rating, _ ->
-            openFlashLight(true, rating.toInt())
-        }
-        val ratingToSet = 1
-        myRatingBar.rating = ratingToSet.toFloat()
+
+        val seekBar = findViewById<View>(R.id.seekBar) as SeekBar
+        seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seekBar: SeekBar, progress: Int,
+                fromUser: Boolean
+            ) {
+                openFlashLight(true, progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
+
+        seekBar.progress = 1
     }
 
     override fun onDestroy() {
         super.onDestroy()
         openFlashLight(false, 1)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        openFlashLight(false, 1)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        openFlashLight(false, 1)
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        openFlashLight(true,1)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        openFlashLight(true,1)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        openFlashLight(true, 1)
     }
 
     private fun openFlashLight(flashLightStatus: Boolean, rating: Int) {
